b07bfec92076129cf981ee3d35c08a712d18a603
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-04 00:50:59 +0530

    Added UC2 Files

--- src/Regex.java
+++ src/Regex.java
@@ -6,11 +6,21 @@ public class Regex {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the First Name");
         String firstName = sc.nextLine();
-        String regex = "^[A-Z]{1}[a-z]{3,}$";
-        boolean result = firstName.matches(regex);
-        if (result)
+        String regex1 = "^[A-Z]{1}[a-z]{3,}$";
+        boolean result1 = firstName.matches(regex1);
+        if (result1) {
             System.out.println("First name is valid");
-        else
+        }
+        else {
             System.out.println("First name is Invalid");
+        }
+        System.out.println("Enter the Last Name");
+        String lastName = sc.nextLine();
+        String regex2 = "^[A-Z]{1}[a-z]{2,}$";
+        boolean result2 = lastName.matches(regex2);
+        if (result2)
+            System.out.println("Last name is valid");
+        else
+            System.out.println("Last name is Invalid");
     }
 }
